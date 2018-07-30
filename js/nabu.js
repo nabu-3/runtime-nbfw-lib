1ceffe0ab3a081d4a0e1d82ed5a761924c846f8e
Rafael Gutiérrez <ragutimar@gmail.com>
2018-07-30 10:05:05 +0200

    Update phone validation regex

--- js/nabu.js
+++ js/nabu.js
@@ -129,7 +129,7 @@ Nabu.prototype = {
     },
 
     isValidPhoneNumber: function (expression) {
-        var filter = /^\+{0,1}[0-9]+[0-9 ]*[0-9]+$/gm;
+        var filter = /^(\+?[0-9\ \-]{8,}[0-9])$/gm;
         return filter.test(expression);
     },
 
